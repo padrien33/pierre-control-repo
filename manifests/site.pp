7b8d637b2d9b993b548a20a8857d6b6eb8a3d0ea
padrien33 <30621172+padrien33@users.noreply.github.com>
2017-09-19 11:05:21 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -24,17 +24,7 @@ File { backup => false }
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
-if $::kernel == 'windows' {
-  Package { provider => chocolatey, }
-}
 
-# OR
-
-case $operatingsystem {
-  'windows': {
-    Package { provider => chocolatey, }
-  }
-}
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
