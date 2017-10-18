4eab5723b262e4a3a0d9956b9004007912649eac
padrien33 <30621172+padrien33@users.noreply.github.com>
2017-10-18 13:25:40 +0100

    Create bootstrap_master.sh

---
+++ scripts/bootstrap_master.sh
@@ -0,0 +1,133 @@
+#/bin/bash
+server_name=$1
+is_ec2=$2
+
+function ec2_setup {
+  setenforce 0
+  sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+  ntpdate -u 0.amazon.pool.ntp.org
+  /sbin/parted -s /dev/xvdb mklabel gpt
+  /sbin/parted -s /dev/xvdb mkpart primary 0% 100%
+  /sbin/mkfs.ext4 /dev/xvdb1
+  mount /dev/xvdb1 /opt
+  echo "/dev/xvdb1              /opt                    ext4    defaults        0 2" >> /etc/fstab
+  lsblk
+  /bin/echo "preserve_hostname: true" >> /etc/cloud/cloud.cfg
+}
+
+function setup_networking {
+  hostnamectl set-hostname $server_name
+  echo "127.0.0.1 $server_name puppet localhost.localdomain localhost" > /etc/hosts
+}
+
+function copy_ssh_keys {
+  mkdir -p /etc/puppetlabs/puppetserver/ssh
+  if [ -d /vagrant/keys ]
+    then
+      echo "Vagrant keys directory exists. Copying them in place"
+      cp /vagrant/keys/id_rsa /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa
+      cp /vagrant/keys/id_rsa.pub /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa.pub
+      mkdir -p ~/.ssh
+      echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+      cp /vagrant/keys/id_rsa ~/.ssh/id_rsa
+      cp /vagrant/keys/id_rsa.pub ~/.ssh/id_rsa.pub
+    else
+      echo "Vagrant keys directory does not exists. Creating them"
+      ssh-keygen -t rsa -b 4096 -N"" -f /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa
+  fi
+}
+
+
+function setup_hiera_pe {
+  sleep 15
+  /opt/puppetlabs/bin/puppet apply -e "include profile::puppet::hiera"
+  /opt/puppetlabs/bin/puppetserver gem install hiera-eyaml
+  if [ -f /vagrant/keys/private_key.pkcs7.pem ]
+    then
+      rm /etc/puppetlabs/puppet/keys/*
+      cp /vagrant/keys/private_key.pkcs7.pem /etc/puppetlabs/puppet/keys/.
+      cp /vagrant/keys/public_key.pkcs7.pem /etc/puppetlabs/puppet/keys/.
+  fi
+}
+
+function run_puppet {
+  cd /
+  /opt/puppetlabs/bin/puppet agent -t
+}
+
+function download_pe {
+  yum -y install wget pciutils
+  wget https://s3.amazonaws.com/pe-builds/released/2017.2.1/puppet-enterprise-2017.2.1-el-7-x86_64.tar.gz
+  tar -xzf puppet-enterprise-2017.2.1-el-7-x86_64.tar.gz -C /tmp/
+}
+
+function install_pe {
+  mkdir -p /etc/puppetlabs/puppet
+  cat > /etc/puppetlabs/puppet/csr_attributes.yaml << YAML
+extension_requests:
+    pp_role:  puppetmaster
+YAML
+
+  cat > /tmp/pe.conf << FILE
+"console_admin_password": "puppetlabs"
+"puppet_enterprise::puppet_master_host": "%{::trusted.certname}"
+"puppet_enterprise::profile::master::code_manager_auto_configure": true
+"puppet_enterprise::profile::master::r10k_remote": "https://github.com/mtyates/demo-control-repo.git"
+"puppet_enterprise::profile::master::r10k_private_key": "/etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa"
+FILE
+  /tmp/puppet-enterprise-2017.2.1-el-7-x86_64/puppet-enterprise-installer -c /tmp/pe.conf
+  chown pe-puppet:pe-puppet /etc/puppetlabs/puppetserver/ssh/id-*
+}
+
+function add_pe_users {
+  /opt/puppetlabs/bin/puppet module install pltraining-rbac
+  cat > /tmp/user.pp << FILE
+rbac_user { 'deploy':
+    ensure       => 'present',
+    name         => 'deploy',
+    display_name => 'deployment user account',
+    email        => 'matthew.yates@puppet.com',
+    password     => 'puppetlabs',
+    roles        => [ 'Code Deployers' ],
+}
+FILE
+  /opt/puppetlabs/bin/puppet apply /tmp/user.pp
+  rm /tmp/user.pp
+
+  /opt/puppetlabs/bin/puppet-access login deploy --lifetime=1y << TEXT
+puppetlabs
+TEXT
+}
+
+function deploy_code_pe {
+  /opt/puppetlabs/bin/puppet-code deploy production -w
+}
+
+if [ $is_ec2 == 'true' ];
+  then
+    ec2_setup
+fi
+
+# Need gem install of aws bits and hiera-eyaml
+function gems {
+  /opt/puppetlabs/puppet/bin/gem install aws-sdk-core retries
+  /opt/puppetlabs/puppet/bin/gem install hiera-eyaml
+}
+
+# Optional autosigner.conf configuration
+function autosigner {
+  /bin/echo "*" >> /etc/puppetlabs/puppet/autosign.conf
+  systemctl restart pe-pupppetserver
+}
+
+
+setup_networking
+download_pe
+copy_ssh_keys
+install_pe
+gems
+add_pe_users
+deploy_code_pe
+setup_hiera_pe
+run_puppet
+autosigner
